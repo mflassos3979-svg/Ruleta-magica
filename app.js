7cd410d95cdc144cd77b046d9d92bb00d9ad16ca
mflassos3979-svg <mflassos3979@gmail.com>
2025-08-13 04:36:19 -0500

    hola mundo

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let numeroSecreto=6;
+let numeroUsuario = prompt("Me indicas un numero del 1 al 10  porfavor:");
+console.log(numeroUsuario);
+if(numeroUsuario== numeroSecreto){
+    alert('Acertaste el numero');
+}
