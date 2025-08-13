c03053205dab82e24959baf307b101f4be408521
mflassos3979-svg <mflassos3979@gmail.com>
2025-08-13 04:39:17 -0500

    hola mundo 2

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ let numeroUsuario = prompt("Me indicas un numero del 1 al 10  porfavor:");
 console.log(numeroUsuario);
 if(numeroUsuario== numeroSecreto){
     alert('Acertaste el numero');
+    
 }
