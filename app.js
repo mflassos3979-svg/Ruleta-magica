9794fabd849f3ba0314e63714e40f3caedd06be6
mflassos3979-svg <mflassos3979@gmail.com>
2025-08-13 05:09:06 -0500

    Corrige error de sintaxis en app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let numeroSecreto=6;
 let numeroUsuario = prompt("Me indicas un numero del 1 al 10  porfavor:");
 console.log(numeroUsuario);
 if(numeroUsuario== numeroSecreto){
-    alert('Acertaste el numero');
+    alert('Acertaste el numero'); 
 }
